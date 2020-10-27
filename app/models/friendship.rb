3eb51c24effe2d9988396678723240a50f062551
Clayton Siby <claytonsiby@gmail.com>
2020-10-27 15:13:22 +0200

    Add associations between user & friendship models

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,7 @@
 class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
+
+  scope :friends, -> { where(confirmed: true) }
+  scope :non_friends, -> { where(confirmed: false) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,25 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships, dependent: :destroy
+
+  has_many :friend_sent, class_name: 'Friendship',
+                         foreign_key: 'user_id',
+                         inverse_of: 'user',
+                         dependent: :destroy
+
+  has_many :friend_requests, class_name: 'Friendship',
+                             foreign_key: 'friend_id',
+                             inverse_of: 'friend',
+                             dependent: :destroy
+
+  has_many :friends, -> { merge(Friendship.friends) },
+            through: :friend_sent, source: 'friend'
+
+  has_many :pending_requests, -> { merge(Friendship.non_friends) },
+            through: :friend_sent, source: 'friend'
+
+  has_many :received_requests, -> { merge(Friendship.non_friends) },
+           through: :friend_requests, source: 'user'
 end
